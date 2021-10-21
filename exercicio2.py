2f79c8fbced9bd7bdf7d5b789b85104ffcd95c75
Shouko3613 <79612430+bfideles@users.noreply.github.com>
2021-10-20 23:52:32 -0300

    Add files via upload

---
+++ exercicio2.py
@@ -0,0 +1,16 @@
+idade = int(input('Insira sua idade:'))
+
+if idade <= 7:
+    print('Categoria: Infantil A')
+
+elif idade <= 10:
+    print('Categoria: Infantil B')
+
+elif idade <= 13:
+    print('Categoria: Infantil A')
+
+elif idade <= 17:
+    print('Categoria: Infantil B')
+
+else:
+    print('Categoria: Adulto')
